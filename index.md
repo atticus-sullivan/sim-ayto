b857dd0eb32b1602054d5c2ef89e7a84a7d47393
Lukas <oss.heindl+git@protonmail.com>
2024-08-17 00:48:32 +0200

    first tries to plot with charming with rust (no need for latex and also responsive as html page)

---
+++ index.md
@@ -0,0 +1,17 @@
+# Berechnete Ergebnisse
+
+- [s01]()
+- [s01r]()
+- [s02]()
+- [s02r]()
+- [s03]()
+- [s03r]()
+- [s04]()
+- [s04r]()
+- [s05]()
+
+# Vergleich
+
+- bzgl Matchingnights
+- bzgl Matchboxes
+- bzgl verbleibende Möglichkeiten
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -22,10 +22,15 @@ mod ruleset;
 
 use crate::game::Game;
 
+use charming::component::{Legend, Toolbox};
+use anyhow::{ensure, Context, Result};
+use charming::element::{Tooltip, Trigger};
 use clap::Parser;
-use std::collections::HashMap;
+use std::{collections::HashMap, path::Path};
 use std::path::PathBuf;
 use std::time::Instant;
+
+use charming::{component::Axis, element::AxisType, series::Line, Chart, HtmlRenderer};
 
 // TODO code review (try with chatGPT)
 
@@ -55,9 +60,78 @@ struct Cli {
     only_check: bool,
 }
 
+fn test_charming() -> Chart {
+    Chart::new()
+        .x_axis(
+            Axis::new()
+                .type_(AxisType::Category)
+                .data(vec!["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]),
+        )
+        .y_axis(Axis::new().type_(AxisType::Value))
+        .series(Line::new().name("a").data(vec![150, 230, 224, 218, 135, 147, 260]))
+        .series(Line::new().name("b").data(vec![300, 460, 448, 436, 270, 294, 520]))
+        .legend(Legend::new().data(vec!["a", "b"]))
+        .tooltip(Tooltip::new().trigger(Trigger::Axis))
+}
+
+use walkdir::WalkDir;
+
+type OtherDataEntry = (u8, f64);
+struct OtherData {
+    mb: Vec<OtherDataEntry>,
+    mn: Vec<OtherDataEntry>,
+    all: Vec<OtherDataEntry>,
+}
+
+fn build_stats_graph() -> Result<Chart> {
+    let mut chart = Chart::new()
+        .y_axis(Axis::new().type_(AxisType::Value))
+        .tooltip(Tooltip::new().trigger(Trigger::Axis))
+    ;
+
+    for entry in WalkDir::new("../")
+        .max_depth(2)
+        .min_depth(2)
+        .into_iter()
+        .filter_entry(|e| {
+            e.file_name().to_str().map_or(false, |e| (e.starts_with("s") || e.starts_with("us")) && e.ends_with(".csv")) &&
+            e.metadata().map_or(false, |e| e.is_file())
+        }).filter_map(|e| e.ok())
+    {
+        let mut o = OtherData{mb: Vec::new(), mn: Vec::new(), all: Vec::new()};
+        let field:&mut Vec<OtherDataEntry>;
+        let n = entry.file_name().to_str().unwrap();
+
+        if n.ends_with("_MB.csv") {
+            field = &mut o.mb;
+        } else if n.ends_with("_MN.csv"){
+            field = &mut o.mn;
+        } else {
+            // skip that file
+            continue
+        }
+
+        let mut rdr = csv::Reader::from_path(entry.path())?;
+        for result in rdr.deserialize() {
+            let record: OtherDataEntry = result?;
+            field.push(record);
+        }
+        chart = chart.series(Line::new().name("a").data(field));
+
+        println!("{} {:?}", entry.path().display(), entry.file_name());
+    }
+
+    Ok(chart)
+}
+
 fn main() {
     let args = Cli::parse();
     let mut g = Game::new_from_yaml(&args.yaml_path, &args.stem).expect("Parsing failed");
+
+    let mut render = HtmlRenderer::new("title", 500, 500);
+    let _ = render.save(&build_stats_graph().unwrap(), Path::new("./x.html"));
+
+    return;
 
     if args.only_check {
         return;
