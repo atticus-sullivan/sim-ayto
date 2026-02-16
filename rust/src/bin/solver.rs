3b72e3e32b9f882bb6ab3d1e31841b1687895f83
Lukas <oss.heindl+git@protonmail.com>
2026-02-16 20:51:36 +0100

    license (1) -- remove headers

--- rust/src/bin/solver.rs
+++ rust/src/bin/solver.rs
@@ -1,21 +1,3 @@
-/*
-sim_ayto
-Copyright (C) 2026  Lukas Heindl
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 // TODO: grep for "expect"/"unwrap" and fixup (or ignore) -- issue might be that that's now
 // different threads
 //
