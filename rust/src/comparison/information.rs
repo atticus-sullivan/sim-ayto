dd24de71fa405ee6283436d0f20e2c0f51544840
Lukas <oss.heindl+git@protonmail.com>
2026-02-17 14:45:20 +0100

    formatting

--- rust/src/comparison/information.rs
+++ rust/src/comparison/information.rs
@@ -8,10 +8,7 @@ use crate::comparison::CmpData;
 ///
 /// `cmp_data` is expected to be a vector of `(ruleset_name, CmpData)` pairs.
 /// Returns a vector of `(tab_title, inline_html_string)` ready to be embedded.
-pub fn build_information_plots(
-    cmp_data: &[(String, CmpData)],
-    theme: u8,
-) -> Vec<(String, String)> {
+pub fn build_information_plots(cmp_data: &[(String, CmpData)], theme: u8) -> Vec<(String, String)> {
     let palette = lut_theme(theme);
     let layout = plotly_gen_layout(palette);
 
